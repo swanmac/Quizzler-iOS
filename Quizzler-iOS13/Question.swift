cd4d58987d5fd0999d26169fcfbcfda0dd7f39aa
Greg Swan <gmswan99@gmail.com>
2023-10-01 16:08:55 -0400

    add swift file question

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,15 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Greg Swan on 10/1/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+}
+
